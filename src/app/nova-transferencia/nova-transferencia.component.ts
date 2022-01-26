730bd3c72871bc1694a0ca61f28997701cd8599b
CoelhoRafael <rafael.coelho@bandtec.com.br>
2022-01-25 22:25:11 -0300

    adicionando projeto bytebank

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,37 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { TransferenciaService } from '../services/transferencia.service';
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
+})
+export class NovaTransferenciaComponent {
+
+constructor(private service: TransferenciaService){}
+
+  valor: number;
+  destino: number;
+
+  transferir() {
+    console.log('Solicitada nova transferência');
+
+    const valorEmitir = {
+       valor: this.valor,
+       destino: this.destino
+    };
+
+    this.service.adicionar(valorEmitir).subscribe(
+      (resultado) => {
+        console.log(resultado);
+        this.limparCampos();
+      },
+      (error) => console.error(error)
+    );
+  }
+
+  limparCampos(){
+    this.valor = 0;
+    this.destino = 0;
+  }
+}
